78fa956f7f2676b30b446af4e703163abc11cb5e
Nour Amr <nouramr487@gmail.com>
2022-11-08 19:50:00 +0200

    Project Structure

---
+++ src/main/java/com/trip/tripScheduling/TripSchedulingApplication.java
@@ -0,0 +1,13 @@
+package com.trip.tripScheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripSchedulingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripSchedulingApplication.class, args);
+	}
+
+}
